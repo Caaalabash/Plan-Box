3259780024f722a2127a1e63097a5115267c88dd
Caaalabash <lilongyao@lilongyaodeMacBook-Pro.local>
2019-01-04 11:55:01 +0800

    doc: gitignore

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -5,10 +5,6 @@ import testAvatar from '../../assets/mock/avatar.jpeg'
 
 export default class TaskCard extends Component {
 
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     const { taskId, taskDesc, taskOwner, taskTotalTime, taskUseTime } = this.props
     const taskProgress = (taskUseTime / taskTotalTime * 100).toFixed(1) + '%'
