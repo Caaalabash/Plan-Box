59bc57cbbceec531c95497a39bb7dd2a17526370
Caaalabash <1121062986@qq.com>
2019-03-13 10:41:29 +0800

    fix: 错误请求body

--- egg-server/app/extend/context.js
+++ egg-server/app/extend/context.js
@@ -1,33 +1,36 @@
 module.exports = {
-  sendRequest(url, option = {}) {
-    let options = { dataType: 'json', ...option }
-    return this.curl(url, options)
+  sendRequest(url, option) {
+    return this.curl(url, { dataType: 'json', contentType: 'json', ...option })
   },
 
   $get(url, query = null, headers = null) {
-    let options = { timeout: [5000, 20000] }
+    const options = { timeout: [5000, 20000] }
     query && (options.data = query)
     headers && (options.headers = headers)
+
     return this.sendRequest(url, options)
   },
 
   $post(url, body = null, headers = null) {
-    let options = { method: 'POST' }
+    const options = { method: 'POST' }
     body && (options.data = body)
     headers && (options.headers = headers)
+
     return this.sendRequest(url, options)
   },
 
   $put(url, body = null, headers = null) {
-    let options = { method: 'PUT' }
+    const options = { method: 'PUT' }
     body && (options.data = body)
     headers && (options.headers = headers)
+
     return this.sendRequest(url, options)
   },
 
   $delete(url, headers = null) {
-    let options = { method: 'DELETE' }
+    const options = { method: 'DELETE' }
     headers && (options.headers = headers)
+
     return this.sendRequest(url, options)
   },
 }
--- egg-server/app/service/oauth.js
+++ egg-server/app/service/oauth.js
@@ -5,12 +5,17 @@ class OauthService extends require('egg').Service {
     this.response =  ctx.helper.response
   }
 
-  async getGithubInfo(code) {
+  async getGithubInfo({ code }) {
     if (!code) return
-    const { data } = await this.ctx.$post(`https://github.com/login/oauth/access_token`, { client_id: process.env.GITHUB_ID, client_secret: process.env.GITHUB_SECRETID, code }, { Accept: 'application/json' })
-    if (data.error) return this.response(this.config.errorCode, { data, a: process.env.GITHUB_ID, b: process.env.GITHUB_SECRETID }, '授权失败')
+    const { data } = await this.ctx.$post(`https://github.com/login/oauth/access_token`, {
+      client_id: process.env.GITHUB_ID,
+      client_secret: process.env.GITHUB_SECRETID,
+      code
+    })
 
+    if (data.error) return this.response(this.config.errorCode, data, '授权失败')
     const userInfo = await this.ctx.$get(`https://api.github.com/user?access_token=${data.access_token}`)
+
     return this.response(this.config.successCode, userInfo.data, '授权成功')
   }
 }
