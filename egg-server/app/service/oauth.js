5d617dee16182c6884ecff031fe1d4f3d6cf912a
Caaalabash <1121062986@qq.com>
2019-03-13 09:46:01 +0800

    test: 测试授权失败

--- egg-server/app/service/oauth.js
+++ egg-server/app/service/oauth.js
@@ -8,7 +8,7 @@ class OauthService extends require('egg').Service {
   async getGithubInfo(code) {
     if (!code) return
     const { data } = await this.ctx.$post(`https://github.com/login/oauth/access_token`, { client_id: process.env.GITHUB_ID, client_secret: process.env.GITHUB_SECRETID, code }, { Accept: 'application/json' })
-    if (data.error) return this.response(this.config.errorCode, {}, '授权失败')
+    if (data.error) return this.response(this.config.errorCode, { data, a: process.env.GITHUB_ID, b: process.env.GITHUB_SECRETID }, '授权失败')
 
     const userInfo = await this.ctx.$get(`https://api.github.com/user?access_token=${data.access_token}`)
     return this.response(this.config.successCode, userInfo.data, '授权成功')
